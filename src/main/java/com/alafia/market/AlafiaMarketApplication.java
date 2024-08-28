76f46be76422d9d99ecccd34033ef28dada6c5d7
Andres Gonzalez <andrewgonzlez@gmail.com>
2024-08-28 18:52:37 -0500

    Connecting DB and adding Entities

---
+++ src/main/java/com/alafia/market/AlafiaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.alafia.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlafiaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlafiaMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alafia/market/AlafiaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alafia.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlafiaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
